389cc735aef4075bb6d2156a3d8c251d367dc9c7
pauldubois98 <pauldubois98@gmail.com>
2021-04-16 12:34:37 +0200

    added percolation plots

---
+++ docs/percolation.js
@@ -0,0 +1,46 @@
+var graph =  document.getElementById("graph");
+
+function activate(e){
+    var c = e.parentElement.childNodes;
+    var j;
+    for(j = 1; j<c.length; j+=2){
+        c[j].classList.remove("active");
+    }
+    e.classList.add("active");
+    updateGraph();
+}
+
+function updateGraph(){
+    var path = "https://github.com/pauldubois98/PercolationFractalsStudy/raw/main/data_visualization/percolation/";
+
+    if(document.getElementById("dimension").classList[0]=="active"){
+        path += "dimension"
+    }
+    if(document.getElementById("density").classList[0]=="active"){
+        path += "density"
+    }
+
+    if(document.getElementById("1D").classList[0]=="active"){
+        path += "_1D"
+    }
+    if(document.getElementById("2D").classList[0]=="active"){
+        path += "_2D"
+    }
+    if(document.getElementById("3D").classList[0]=="active"){
+        path += "_3D"
+    }
+    if(document.getElementById("4D").classList[0]=="active"){
+        path += "_4D"
+    }
+    if(document.getElementById("5D").classList[0]=="active"){
+        path += "_5D"
+    }
+    
+    path += ".png"
+    //console.log(path);
+    graph.src = path;
+}
+
+
+
+
