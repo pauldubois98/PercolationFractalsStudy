fb2fa87f1ad007853e419e9a86670b1e7e40c8a6
pauldubois98 <pauldubois98@gmail.com>
2021-04-17 09:49:53 +0200

    docs: added blob page

---
+++ docs/blob.js
@@ -0,0 +1,55 @@
+var graph =  document.getElementById("graph");
+
+function activate(e){
+    var c = e.parentElement.childNodes;
+    var j;
+    for(j = 1; j<c.length; j+=2){
+        c[j].classList.remove("active");
+    }
+    e.classList.add("active");
+    updateGraph();
+}
+
+function updateGraph(){
+    var path = "https://github.com/pauldubois98/PercolationFractalsStudy/raw/main/data_visualization/";
+    if(document.getElementById("2D").classList[0]=="active"){
+        path += "blob_2D/"
+    }
+    if(document.getElementById("3D").classList[0]=="active"){
+        path += "blob_3D/"
+    }
+    path += "blob_";
+    if(document.getElementById("step").classList[0]=="active"){
+        path += "step_"
+    }
+    if(document.getElementById("distance").classList[0]=="active"){
+        path += "dist_"
+    }
+    if(document.getElementById("area").classList[0]=="active"){
+        path += "area_"
+    }
+    if(document.getElementById("volume").classList[0]=="active"){
+        path += "vol_"
+    }
+    if(document.getElementById("2D").classList[0]=="active"){
+        path += "2D"
+        if(document.getElementById("d").classList[0]=="active"){
+            path += ""
+        }
+        if(document.getElementById("n").classList[0]=="active"){
+            path += "_ter"
+        }
+    }
+    if(document.getElementById("3D").classList[0]=="active"){
+        path += "3D"
+        if(document.getElementById("d").classList[0]=="active"){
+            path += ""
+        }
+        if(document.getElementById("n").classList[0]=="active"){
+            path += "_bis"
+        }
+    }
+    path += ".png"
+    //console.log(path);
+    graph.src = path;
+}
